8088124a5c19f52c02bc8a2fc00f8e49aa3d4eef
KleprlikJan <kleprlikjan@gmail.com>
2022-06-14 21:15:28 +0200

    Add project files.

---
+++ Vimal/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+using Vimal.Helpers;
+using Vimal.Services;
+using Vimal.Views;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+
+namespace Vimal.ViewModels
+{
+    // TODO: Add other settings as necessary. For help see https://github.com/microsoft/TemplateStudio/blob/main/docs/UWP/pages/settings.md
+    public class SettingsViewModel : ObservableObject
+    {
+        private ElementTheme _elementTheme = ThemeSelectorService.Theme;
+        private RelayCommand _closeSettingsCommand;
+
+        public RelayCommand CloseSettingsCommand => _closeSettingsCommand ?? (_closeSettingsCommand = new RelayCommand(CloseSettigns));
+
+        public ElementTheme ElementTheme
+        {
+            get { return _elementTheme; }
+
+            set { SetProperty(ref _elementTheme, value); }
+        }
+
+        private string _versionDescription;
+
+        public string VersionDescription
+        {
+            get { return _versionDescription; }
+
+            set { SetProperty(ref _versionDescription, value); }
+        }
+
+        private ICommand _switchThemeCommand;
+
+        public ICommand SwitchThemeCommand
+        {
+            get
+            {
+                if (_switchThemeCommand == null)
+                {
+                    _switchThemeCommand = new RelayCommand<ElementTheme>(
+                        async (param) =>
+                        {
+                            ElementTheme = param;
+                            await ThemeSelectorService.SetThemeAsync(param);
+                        });
+                }
+
+                return _switchThemeCommand;
+            }
+        }
+
+        public SettingsViewModel()
+        {
+        }
+
+        public async Task InitializeAsync()
+        {
+            VersionDescription = GetVersionDescription();
+            await Task.CompletedTask;
+        }
+
+        private string GetVersionDescription()
+        {
+            var appName = "AppDisplayName".GetLocalized();
+            var package = Package.Current;
+            var packageId = package.Id;
+            var version = packageId.Version;
+
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        private void CloseSettigns()
+        {
+            NavigationService.Navigate(typeof(MainPage), null);
+        }
+    }
+}
---
+++ Vimal/Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Vimal.ViewModels;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Vimal.Views
+{
+    // For more info about the TabView Control see
+    // https://docs.microsoft.com/uwp/api/microsoft.ui.xaml.controls.tabview?view=winui-2.2
+    // For other samples, get the XAML Controls Gallery app http://aka.ms/XamlControlsGallery
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            Window.Current.SetTitleBar(CustomDragRegion);
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (FlowDirection == FlowDirection.LeftToRight)
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayRightInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayLeftInset;
+            }
+            else
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayLeftInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayRightInset;
+            }
+
+            CustomDragRegion.Height = ShellTitlebarInset.Height = sender.Height;
+        }
+    }
+}
