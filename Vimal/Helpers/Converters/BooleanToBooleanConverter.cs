6c2bc10980acbbffbc148253d61872b62edd3e5c
KleprlikJan <kleprlikjan@gmail.com>
2022-06-16 21:59:02 +0200

    Move ValueConverters

--- Vimal/Helpers/Converters/BooleanToBooleanConverter.cs
+++ Vimal/Helpers/Converters/BooleanToBooleanConverter.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
 
-namespace Vimal.Helpers
+namespace Vimal.Helpers.Converters
 {
     /// <summary>
     /// NOT operation over boolean
