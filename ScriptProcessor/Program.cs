9e023df6c01a7c875763e43dcf0108931ecbf13c
KleprlikJan <kleprlikjan@gmail.com>
2022-06-16 21:44:12 +0200

    Code cleanup & add docs

--- ScriptProcessor/Program.cs
+++ ScriptProcessor/Program.cs
@@ -1,65 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.AppService;
 using Windows.Foundation.Collections;
 
 
-#region connection
-
+// Establish connection
 var connection = new AppServiceConnection();
 connection.AppServiceName = "SampleInteropService";
 connection.PackageFamilyName = Package.Current.Id.FamilyName;
-
-connection.RequestReceived += Connection_RequestReceived;
-
-void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
-{
-    Console.WriteLine("Request received");
-}
-
-connection.ServiceClosed += Connection_ServiceClosed;
-
-void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
-{
-    Console.WriteLine("Service closed");
-}
 
 AppServiceConnectionStatus status = await connection.OpenAsync();
 if (status != AppServiceConnectionStatus.Success)
 {
     // something went wrong ...
-    Console.WriteLine("Connection failed");
-    Console.WriteLine(status);
+    Debug.WriteLine("Connection failed");
+    Debug.WriteLine(status);
 }
 
-#endregion
 
-
+// Get script settings
 var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-
 var scriptPath = Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\script.kts";
-
 string compilerPath = localSettings.Values["compilerPath"] as string ?? "";
-string v2 = localSettings.Values["saveToFile"] as string ?? "";
 string scriptData = localSettings.Values["scriptData"] as string ?? "";
 string language = localSettings.Values["language"] as string ?? "kotlin";
+int repetitions = localSettings.Values["repetitions"] as int? ?? 1;
 
+
+//Pick language
 string compilerFileName = "";
 if (language == "kotlin")
     compilerFileName = "kotlinc.bat";
 if (language == "swift")
     compilerFileName = "swift";    
 
-int repetitions = localSettings.Values["repetitions"] as int? ?? 1;
 
-//write the script into a file
+//Write the script into a file
 await File.WriteAllTextAsync(scriptPath, scriptData);
 
-int exitCode = 0;
 
+//Execute script
+int exitCode = 0;
 for (int i = 0; i < repetitions; i++)
 {
+    //Prepare process with script
     Process proc = new Process
     {
         StartInfo = new ProcessStartInfo
@@ -76,7 +60,8 @@ for (int i = 0; i < repetitions; i++)
     };
     
     proc.Start();
-    
+
+    //Send output to UWP app
     while (!proc.StandardOutput.EndOfStream)
     {
         string line = proc.StandardOutput.ReadLine();
@@ -86,8 +71,10 @@ for (int i = 0; i < repetitions; i++)
         connection.SendMessageAsync(request);
 
     }
+    
     proc.WaitForExit();
-
+    
+    //Send error to UWP app
     if (proc.ExitCode != 0)
     {
         //Write error to output
@@ -101,17 +88,19 @@ for (int i = 0; i < repetitions; i++)
     }
     exitCode = proc.ExitCode;
 
-    // Sleep to make sure the output is flushed
+
+    //Send number of repetitions to UWP app
+    //Sleep to make sure the output is flushed
     Thread.Sleep(50);
     ValueSet requestRepetition = new ValueSet();
     requestRepetition.Add("REP", (1+i).ToString());
 
     connection.SendMessageAsync(requestRepetition);
     Thread.Sleep(50);
-
 }
 
-//Add thread sleep so all messages go through before the process kills itself
+//Send exit code to UWP app
+//Sleep so all messages go through before the process kills itself
 Thread.Sleep(50);
 ValueSet requestCode = new ValueSet();
 requestCode.Add("CODE", exitCode.ToString());
